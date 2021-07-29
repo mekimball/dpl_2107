9f16d2d17c2a250975971361ea9af744861df64e
Matthew Kimball <mekimba6@gmail.com>
2021-07-29 14:57:02 -0600

    adds some library functionality

--- lib/library.rb
+++ lib/library.rb
@@ -15,4 +15,12 @@ class Library
       @books << author.books
     end
   end
+
+  def publication_time_frame_for(book)
+    book.each do |books|
+      first_publication = books.publication_year.min_by
+    end
+    first_publication
+  end
+
 end
